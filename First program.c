08b971eb31b2e227576027258f96c924bb3c2804
Hari-21 <77657443+Hari-21@users.noreply.github.com>
2021-08-03 10:07:03 +0530

    Update First program.c

--- First program.c
+++ First program.c
@@ -1,14 +1,1 @@
-#include<iostream.h>
-#include<conio.h>
-int main()
-{
-    int a,b;
-    cout<<"Enter the number";
-    cin>>a;
-    cout<<"Enter another number";
-    cin>>b;
-    int c=a+b;
-    cout<<"The addition of two numbers is:"<<c;
-    return 0;
 
-}
